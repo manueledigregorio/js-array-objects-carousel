582d53ba66eba98e4c774289a4e85bd3375ddabb
manueledigregorio <dig.manuele@gmail.com>
2023-09-22 17:51:19 +0200

    [add] processo readme

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Riprendiamo l’esercizio carosello e rifacciamolo, questa volta usando gli ogge
 Alla slide aggiungere un titolo e un testo
 **Bonus 1:**
 Sperimentiamo attraverso l’uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:al click di un bottone o già dall’inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
+
+### PROCESSO 
+1. creiamo un ciclo for each 
+1. prendiamo l'elemento del for each
+1. con il teplate literal spampiamo in pagina
+1. creiamo una classe text metto position absolute cosi da far vedere il testo in primo piaano 
+1. richiamo la classe su js
+1. dopo aggiungiamo e nascondiamo tramite la classe hide
--- js/script.js
+++ js/script.js
@@ -3,26 +3,52 @@ const carouselImg = document.querySelector(".items-wrapper");
 const btnUp = document.querySelector(".bt-top") 
 const btnDown = document.querySelector(".bt-bottom")
 
-// Tutti gli array iniziano da 0
 const images = [
-  "/img/01.webp", // 0
-  "/img/02.webp", // 1
-  "/img/03.webp", // 2
-  "/img/04.webp", // 3
-  "/img/05.webp" // 4
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
 ];
+
+
  // la lunghezza dell'elemento html nel nostro caso sono 5
-for (let i = 0; i < images.length; i++) {
-  carouselImg.innerHTML += `<img src="${images[i]}" class="img-car hiden" />`;
-}
+images.forEach((element) => {
+  carouselImg.innerHTML += `<img src="${element.image}" class="img-car hiden" />
+                            <div class="text hiden">
+                              <h2>${element.title}</h2>
+                              <p>${element.text}</p>
+                            </div>
+                            
+  
+  `;
+});
 
 // Non puoi inserire 1 come posizione iniziale altrimenti andresti a prendere la seconda immagine dell'array
 let carouselIndex = 0;
 
 let carouselImages = document.getElementsByClassName('img-car');
 
-// Prendo la prima immagine (in posizione 0 nell'array) e la mostro
-carouselImages[carouselIndex].classList.remove('hiden')
+let carouselText = document.getElementsByClassName('text');
+
+carouselText[carouselIndex].classList.remove('hiden');
+carouselImages[carouselIndex].classList.remove('hiden');
 
 console.log("Indice carosello: " + carouselIndex);
 console.log("Lunghezza array: " + carouselImages.length)
@@ -30,6 +56,7 @@ console.log("Lunghezza array: " + carouselImages.length)
 btnUp.addEventListener('click', function() {
   // Rimuovo l'immagine attuale prima di cambiare il valore del contatore
   carouselImages[carouselIndex].classList.add('hiden')
+  carouselText[carouselIndex].classList.add('hiden')
 
   carouselIndex--;
 
@@ -39,6 +66,7 @@ btnUp.addEventListener('click', function() {
 
   // Dopo aver cambiato il valore del contatore, mostro la nuova immagine
   carouselImages[carouselIndex].classList.remove('hiden')
+  carouselText[carouselIndex].classList.remove('hiden');
 
   console.log("Indice carosello: " + carouselIndex);
   console.log("Lunghezza array: " + carouselImages.length)
@@ -47,6 +75,7 @@ btnUp.addEventListener('click', function() {
 // Tasto per andare avanti
 btnDown.addEventListener('click', function() {
   carouselImages[carouselIndex].classList.add('hiden')
+  carouselText[carouselIndex].classList.add('hiden')
 
   carouselIndex++;
 
@@ -54,7 +83,8 @@ btnDown.addEventListener('click', function() {
     carouselIndex = 0;
   }
 
-  carouselImages[carouselIndex].classList.remove('hiden')
+  carouselImages[carouselIndex].classList.remove('hiden');
+  carouselText[carouselIndex].classList.remove('hiden');
 
   console.log("Indice carosello: " + carouselIndex);
   console.log("Lunghezza array: " + carouselImages.length)
